c094226f644bd1526d1e58babbf1c54a97a9a98b
Bivekich <biveki-dev@yandex.ru>
2024-12-07 10:07:12 +0300

    auth

---
+++ src/auth/auth.service.ts
@@ -0,0 +1,45 @@
+import {
+  Injectable,
+  ConflictException,
+  UnauthorizedException,
+} from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
+import { UsersService } from '../users/users.service';
+import * as bcrypt from 'bcrypt';
+
+@Injectable()
+export class AuthService {
+  constructor(
+    private usersService: UsersService,
+    private jwtService: JwtService,
+  ) {}
+
+  async validateUser(username: string, password: string): Promise<any> {
+    const user = await this.usersService.findOne(username);
+    if (user && (await bcrypt.compare(password, user.password))) {
+      const { password, ...result } = user;
+      return result;
+    }
+    return null;
+  }
+
+  async login(user: any) {
+    const payload = {
+      username: user.username,
+      sub: user.id,
+      roles: user.roles,
+    };
+    return {
+      access_token: this.jwtService.sign(payload),
+    };
+  }
+
+  async register(username: string, password: string) {
+    const existingUser = await this.usersService.findOne(username);
+    if (existingUser) {
+      throw new ConflictException('Пользователь с таким именем уже существует');
+    }
+    const user = await this.usersService.create(username, password);
+    return this.login(user);
+  }
+}
